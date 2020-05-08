95956ea71d2fd5fb31a9ca21be5cc1fe4c9e08ad
Junseop <gaon0403@gmail.com>
2020-05-08 17:27:27 +0900

    Add progress bar

--- code_work_tester.py
+++ code_work_tester.py
@@ -4,6 +4,7 @@ import glob
 import argparse
 import subprocess
 
+from tqdm import tqdm
 from multiprocessing import Pool
 
 parser = argparse.ArgumentParser()
@@ -11,6 +12,7 @@ parser.add_argument('--multi', type=int, default=4)
 parser.add_argument('--dir', type=str, default='./')
 
 def test_run(pyfile):
+    print(pyfile)
     student_no = re.search('[0-9]{8}', pyfile, re.IGNORECASE).group()
     student_path = os.path.join(os.path.dirname(pyfile), student_no)
     os.makedirs(student_path, exist_ok=True)
@@ -23,7 +25,14 @@ def test_run(pyfile):
 
 def _main(args):
     pool = Pool(processes=args.multi)
-    pool.map(test_run, glob.glob(os.path.join(args.dir, '*.py')))
+    pyfiles = glob.glob(os.path.join(args.dir, '*.py'))
+    with tqdm(total=len(pyfiles)) as pbar:
+        for _ in tqdm(pool.imap_unordered(test_run, pyfiles)):
+            pbar.update()
+    pool.close()
+    pool.join()
+    
+    # pool.map(test_run, glob.glob(os.path.join(args.dir, '*.py')))
         
 
 if __name__ == '__main__':
